e40717f2b875924349e47b3c624926965e7c0f48
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 18:01:53 +1000

    unit test 7
    unit test 7

---
+++ src/bcccp/carpark/exit/Unit testing test 7.java
@@ -0,0 +1,12 @@
+package src.bcccp.carpark.exit;
+
+import bcccp.carpark.Carpark;
+import bcccp.carpark.ICarSensor;
+import bcccp.carpark.IGate;
+
+public class ExitControllerTest7 {
+	ExitController test7= new ExitController();
+	test7.log("hello how are you");
+	
+
+}
