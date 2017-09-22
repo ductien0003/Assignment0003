d7427b7a27e30a332fbf2beaaa0997176fe9a3be
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 21:32:53 +1000

    unit test # 18
    unit testing number 18

---
+++ src/bcccp/carpark/exit/Unit testing test 18.java
@@ -0,0 +1,12 @@
+package src.bcccp.carpark.exit;
+
+import org.junit.Test;
+
+import bcccp.carpark.Carpark;
+import bcccp.carpark.ICarSensor;
+import bcccp.carpark.IGate;
+
+public class ExitControllerTest18{
+	ExitController test18= new ExitController();
+	Test15.carDetected("2222",1)
+}
