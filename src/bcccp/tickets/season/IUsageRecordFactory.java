b91d103e77617680c4b744b8699126ffa7a80bd1
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 17:13:37 +1000

    Integration Testing
    Performed Integration Testing

--- src/bcccp/tickets/season/IUsageRecordFactory.java
+++ src/bcccp/tickets/season/IUsageRecordFactory.java
@@ -3,5 +3,5 @@ package bcccp.tickets.season;
 public interface IUsageRecordFactory {
 
 	public IUsageRecord make(String ticketId, long startDateTime);
-	system.out.println("Integration Testing ");
+	system.out.println("Integration Testing2 ");
 }
