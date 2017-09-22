69fb4006e4813506d1fe82ed16fab046e998818e
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 18:29:01 +1000

    unit test 13
    Tien Huynh unit test 13

---
+++ src/bcccp/carpark/exit/Unit testing test 13.java
@@ -0,0 +1,11 @@
+package src.bcccp.carpark.exit;
+
+import bcccp.carpark.Carpark;
+import bcccp.carpark.ICarSensor;
+import bcccp.carpark.IGate;
+
+public class ExitControllerTest13{
+	ExitController test13= new ExitController();
+	test12.ExitController();
+
+}
