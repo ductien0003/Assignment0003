771621066706e185e68178a30f36d3c7ad9435e0
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 17:07:17 +1000

    a3.Iusagerecordfactory.java
    Performed unti tetsing

--- src/bcccp/tickets/season/IUsageRecordFactory.java
+++ src/bcccp/tickets/season/IUsageRecordFactory.java
@@ -3,5 +3,5 @@ package bcccp.tickets.season;
 public interface IUsageRecordFactory {
 
 	public IUsageRecord make(String ticketId, long startDateTime);
-	system.out.println("Unit Testing");
+	system.out.println("Unit Testing2");
 }
