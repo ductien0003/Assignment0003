e6781bf3356d946bbdd9c991a28007ffd8a9cbdb
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-09-22 13:23:26 +1000

    test 8 Integration testing
    test 8 Integration testing

--- src/bcccp/carpark/exit/Integration test 8.java
+++ src/bcccp/carpark/exit/Integration test 8.java
@@ -1,9 +1,9 @@
 package src.bcccp.carpark.exit;
 
 public class ItegrationTestIExitControllerAndIExitUI {
-	IExitController test8=new IExitController();
+	IExitController test7=new IExitController();
 	System.out.prinln("I'm testing IExitController and IExitUI class");
-	// this is my 8th test, I hope it will work
+	
 	
 	
 }
